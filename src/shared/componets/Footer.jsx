c56898f822a17bfae518b2f3949ca1f4689b9acc
Adrianfer <adrianfermartinez6@gmail.com>
2025-06-12 11:22:44 -0400

    Mejora IU en el Footer

--- src/shared/componets/Footer.jsx
+++ src/shared/componets/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='w-full flex items-center justify-center max-w-3xl mx-auto pt-6 pb-8 px-6 mt-16'>
-      <p>© 2021 - 2025 A|R System. Todos los derechos reservados.</p>
+    <footer className='w-full flex flex-wrap items-center justify-center max-w-3xl mx-auto pt-6 pb-8 px-6 mt-16'>
+      <p className='w-full text-center'>© 2021 - 2025 A|R System.</p>
+      <a href='#' className='w-full text-center underline'>Todos los derechos reservados.</a>
     </footer>
   );
 }
